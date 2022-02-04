0c03f6a2f0dcdf3471d392ccd65953a4d0a29d90
Wensong Zhang <wzhang15@masonlive.gmu.edu>
2022-02-03 19:02:29 -0600

    lab05

---
+++ lab05/01-font-switcher/index.js
@@ -0,0 +1,20 @@
+let currentFont = 1.4;
+
+const makeBigger = () => {
+   // alert('make bigger!');
+   currentFont += 0.2;
+   setFont();
+};
+
+const makeSmaller = () => {
+   currentFont -= 0.2;
+   setFont();
+};
+
+const setFont = () => {
+   document.querySelector('.content').style.fontSize = `${currentFont}em`
+}
+
+document.querySelector('#a1').addEventListener('click', makeBigger);
+document.querySelector('#a2').addEventListener('click', makeSmaller);
+
